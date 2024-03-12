ffe33b3ef09aa0314d30982cbbf4c61203adb3de
Preludex <143441615+Preludex@users.noreply.github.com>
2024-03-12 15:41:48 -0700

    Add files via upload

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,37 @@
+const todoList = ['make dinner', 'wash dishes'];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `
+      <p>
+        ${todo} 
+        <button onclick="
+          todoList.splice(${i}, 1);
+          renderTodoList();
+        ">Delete</button>
+      </p>
+      `;
+    todoListHTML += html;
+  }
+  console.log(todoListHTML);
+
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML
+  }
+
+function addTodo () {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  todoList.push(name);
+  console.log(todoList);
+
+  inputElement.value = '';
+
+  renderTodoList();
+}
